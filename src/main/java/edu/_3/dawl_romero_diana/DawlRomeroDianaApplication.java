fb1b017513212769de38d9736b08cbcc39442599
diaaaaana <didaromaalan2005@gmail.com>
2024-12-16 21:27:55 -0500

    creacion de proyecto de examen final

---
+++ src/main/java/edu/_3/dawl_romero_diana/DawlRomeroDianaApplication.java
@@ -0,0 +1,13 @@
+package edu._3.dawl_romero_diana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawlRomeroDianaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawlRomeroDianaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/_3/dawl_romero_diana/DawlRomeroDianaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu._3.dawl_romero_diana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawlRomeroDianaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
